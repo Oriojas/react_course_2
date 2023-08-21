216bc13ddce3e03dba732e0df5004edcdb78e9bb
oscar50 <oscar@whaleandjaguar.co>
2023-08-20 21:47:55 -0500

    feat: boostrap react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from "./logo.svg";
+import { Button, Accordion, Card } from "react-bootstrap";
 import "./App.css";
-import Saludar from "./components/Saludar";
 
 function App() {
-  const user = {
-    nombre: "Oscar Riojas",
-    edad: 26,
-    color: "Azul",
-  };
-
-  const saludarFn = (name, edad) => {
-    console.log(`Hola ${name}, tiene ${edad} años.`);
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Saludar userInfo={user} saludarFn={saludarFn}></Saludar>
-      </header>
+      <h1>React Boostrap</h1>
+      <Button variant="primary" onClick={() => alert("Esto es un boton")}>
+        Aceptar
+      </Button>
+
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Body>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
