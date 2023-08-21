93772eeba741e0f9e9f8fdd44d38752bafbff111
oscar50 <oscar@whaleandjaguar.co>
2023-08-20 19:09:54 -0500

    feat: create firts component

---
+++ src/components/AdiosMundo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AdiosMundo () {
+
+    return (
+        <div>
+            <h3>Adios</h3>
+        </div>
+    );
+    
+}
---
+++ src/components/HolaMundo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HolaMundo () {
+
+    const miFuncion = () => {
+        console.log("Mi primera función");
+    }
+    
+    return(
+        <div>
+            <h1>Hola Mundo</h1>
+            <h2>Oscar Riojas</h2>
+        </div>
+    );
+}
