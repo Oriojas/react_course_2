be6b3a31132b7df7a82354e0874146aec513ba3a
oscar50 <oscar@whaleandjaguar.co>
2023-08-20 20:58:14 -0500

    feat: json from props

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import "./App.css";
 import Saludar from "./components/Saludar";
 
 function App() {
+  const user = {
+    nombre: "Oscar Riojas",
+    edad: 26,
+    color: "Azul",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Saludar name="Oscar Riojas" age="44"></Saludar>
-        <Saludar name="Angelica Riojas" age="16"></Saludar>
+        <Saludar userInfo={user}></Saludar>
       </header>
     </div>
   );
--- src/components/Saludar.js
+++ src/components/Saludar.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export default function Saludar(props) {
-  console.log(props.name);
+  console.log(props.userInfo);
 
   return (
     <div>
       <h2>
-        Hola {props.name}, tiene {props.age} años
+        Hola {props.userInfo.nombre}, tiene {props.userInfo.edad} años, su color
+        favorito es {props.userInfo.color}
       </h2>
     </div>
   );
