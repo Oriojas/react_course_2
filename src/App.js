0dd09157c2b2e56620bdf40d8fab124cfcb24bb4
oscar50 <oscar@whaleandjaguar.co>
2023-08-21 21:46:20 -0500

    feat: Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [stateCar, setStateCar] = useState(false);
+  const [contar, setContar] = useState(0);
+
+  useEffect(() => {
+    console.log("Total: " + contar);
+  }, [contar]);
 
   const encenderApagar = () => {
     //setStateCar(!stateCar);
     setStateCar((prevValue) => !prevValue); // para enviar el valor de stateCar para otro componente
+    setContar(contar + 1);
   };
 
   return (
     <div className="App">
       <h2>UserState</h2>
       <h3>El carro esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+      <h4>Clicks; {contar}</h4>
       <button onClick={encenderApagar}>Encender / Apagar</button>
     </div>
   );
