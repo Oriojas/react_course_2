566a4ed8c9de3c8fec26c0011875cd512532081f
oscar50 <oscar@whaleandjaguar.co>
2023-08-21 21:39:32 -0500

    feat: Hook useState

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
-import { Button, Accordion, Card } from "react-bootstrap";
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [stateCar, setStateCar] = useState(false);
+
+  const encenderApagar = () => {
+    //setStateCar(!stateCar);
+    setStateCar((prevValue) => !prevValue); // para enviar el valor de stateCar para otro componente
+  };
+
   return (
     <div className="App">
-      <h1>React Boostrap</h1>
-      <Button variant="primary" onClick={() => alert("Esto es un boton")}>
-        Aceptar
-      </Button>
-
-      <Accordion defaultActiveKey="0">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
-          <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Accordion.Body>
-        </Accordion.Item>
-      </Accordion>
+      <h2>UserState</h2>
+      <h3>El carro esta: {stateCar ? "Encendido" : "Apagado"}</h3>
+      <button onClick={encenderApagar}>Encender / Apagar</button>
     </div>
   );
 }
